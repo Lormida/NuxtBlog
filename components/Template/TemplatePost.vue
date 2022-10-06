18fec438e83ef86b30ac5f865719cc7a167c8d20
Lormida <pocketbook.love24@gmail.com>
2022-10-06 14:32:56 +0300

    feat(layout): filter layout

--- components/Template/TemplatePost.vue
+++ components/Template/TemplatePost.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
+    import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/vue'
+
     const filters = ['Art item', 'Art', 'Celebrities', 'Game', 'Music', 'Video', 'Crypto']
     const controlButtons = [
-        {
-            text: 'Filter',
-            color: 'rgba(253,253,253,0.8)',
-            bgColor: 'rgba(15,15,15,.9)',
-            icon: 'i-carbon-filter',
-        },
         {
             text: 'Newest',
             color: 'rgba(253,253,253,0.8)',
@@ -14,6 +10,13 @@
             icon: 'i-carbon-network-overlay',
         },
     ]
+
+    const people = [
+        { id: 1, name: 'Popularity', unavailable: false },
+        { id: 2, name: 'Duration', unavailable: false },
+        { id: 3, name: 'Date', unavailable: false },
+    ]
+    const selectedPerson = ref(people[2])
 </script>
 
 <template>
@@ -41,6 +44,68 @@
             </slot>
             <slot name="controls">
                 <div flex items-center gap-x-10>
+                    <div relative rounded-xl>
+                        <Listbox v-model="selectedPerson">
+                            <ListboxButton
+                                flex
+                                justify-center
+                                gap-x-6
+                                items-center
+                                class="min-w-[200px]"
+                                text="main 2xl"
+                                p="x-6 y-3"
+                                duration-300
+                                hover="bg-main text-anti-main"
+                                bg-anti-main
+                                border-2
+                                border-anti-main
+                                rounded-xl
+                                font-medium
+                            >
+                                <div i-carbon-filter text-2xl></div>
+                                {{ selectedPerson.name }}</ListboxButton
+                            >
+                            <Transition name="list">
+                                <ListboxOptions
+                                    absolute
+                                    w-full
+                                    class="min-w-[200px]"
+                                    border-2
+                                    border-neutral
+                                    top-20
+                                    left-0
+                                >
+                                    <ListboxOption
+                                        v-for="person in people"
+                                        :key="person.id"
+                                        v-slot="{ active, selected }"
+                                        px-4
+                                        py-3
+                                        text-xl
+                                        text-left
+                                        pl-10
+                                        font-medium
+                                        class="min-w-[200px]"
+                                        duration-300
+                                        hover:cursor-pointer
+                                        :value="person"
+                                        as="template"
+                                    >
+                                        <li
+                                            :class="{
+                                                'text-main bg-anti-main': active,
+                                                'text-anti-main bg-main': !active,
+                                                'selected-item': selected,
+                                            }"
+                                        >
+                                            <CheckIcon v-show="selected" />
+                                            {{ person.name }}
+                                        </li>
+                                    </ListboxOption>
+                                </ListboxOptions>
+                            </Transition>
+                        </Listbox>
+                    </div>
                     <VButton
                         v-for="controlButton in controlButtons"
                         :key="controlButton.text"
@@ -67,3 +132,32 @@
         </div>
     </section>
 </template>
+
+<style lang="scss" scoped>
+    .list-enter-active {
+        animation: list-in 0.4s;
+    }
+    .list-leave-active {
+        animation: list-in 0.4s reverse;
+    }
+    @keyframes list-in {
+        0% {
+            opacity: 0;
+            transform: scale(0);
+        }
+        50% {
+            opacity: 0.5;
+            transform: scale(1.25);
+        }
+        100% {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
+
+    .selected-item {
+        transform: scaleX(1.1);
+        color: rgba(var(--emerald-900)) !important;
+        background-color: rgba(var(--emerald-200)) !important;
+    }
+</style>
