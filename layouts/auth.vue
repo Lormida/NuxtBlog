1fa921b1d96d4390698a357ead2074ead11e2260
Lormida <pocketbook.love24@gmail.com>
2022-10-08 23:12:54 +0300

    feat(layout): add sign up page & small refactoring login page

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="container-auth-outer">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .container-auth-outer {
+        @apply min-w-screen h-full min-h-screen flex justify-center items-center;
+        background-image: url('~/assets/imgs/auth-bg2.avif');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
